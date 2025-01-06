581a94a4532b1153ce0abc1c638d2830ea62ffb9
zeyadaverotech <zeyad.hawash1@hotmail.com>
2025-01-06 14:19:04 +0200

    android-app

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
